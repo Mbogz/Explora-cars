20d557a968a612b50a75224a9b3009bdb806aab3
Mbogz <127532929+Mbogz@users.noreply.github.com>
2023-09-06 12:40:58 +0000

    CarDetails

--- components/CarDetails.jsx
+++ components/CarDetails.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios"
+import { Card, Badge, Button, Spinner } from "react-bootstrap";
 
 
-export default function CarDetails = () => {
+export default function CarDetails()  {
   const{id} = useParams();
   const [car, setCar] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -12,7 +13,7 @@ export default function CarDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`https://exploracars.herokuapp.com/api/cars/${id}`)
+      .get(`https://explora-cars-production.up.railway.app/cars/${id}`)
       .then((response) => {
         setCar(response.data);
         setLoading(false);
@@ -26,7 +27,7 @@ export default function CarDetails = () => {
 
     const handleBooking = () => {
       axios
-        .post(`https://exploracars.herokuapp.com/api/bookings`, {
+        .post(`https://explora-cars-production.up.railway.app/bookings`, {
           car_id: car._id,
           user_id: "some_user_id", 
           start_date: "some_start_date",
@@ -39,4 +40,43 @@ export default function CarDetails = () => {
           alert("Something went wrong. Please try again later.");
         });
       };
-}
+
+      if (loading) {
+        return <Spinner animation="border" />;
+      }
+      else if (error){
+        return <p>{error}</p>;
+      };
+      else if (car) {
+        return (
+          <Card>
+            <Card.Img variant="top" src={car.image_url} />
+            <Card.Body>
+              <Card.Title>{car.make}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {car.year} | {car.transmission} | {car.body_type} | {car.category}
+              </Card.Subtitle>
+              <Card.Text>
+                Price per day: ${car.price_per_day}
+                <br />
+                Fuel consumption: {car.fuel_consumption} L/100km
+                <br />
+                Number of seats: {car.number_of_seats}
+                <br />
+                Fuel type: {car.fuel_type}
+              </Card.Text>
+              <Badge variant={car.is_rented ? "danger" : "success"}>
+                {car.is_rented ? "Unavailable" : "Available"}
+              </Badge>
+              <Button variant="primary" onClick={handleBooking} disabled={car.is_rented}>
+                Request Booking
+              </Button>
+            </Card.Body>
+          </Card>
+        );
+      }
+      else {
+        return <p>No car found</p>;
+      }
+    }
+
