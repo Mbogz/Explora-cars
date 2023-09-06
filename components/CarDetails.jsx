619b6bfe4ae91c442ef855693bc3884fc28856ba
Mbogz <127532929+Mbogz@users.noreply.github.com>
2023-09-06 11:12:39 +0000

    Request for booking

--- components/CarDetails.jsx
+++ components/CarDetails.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"
 
-const CarDetails = () => {
+
+export default function CarDetails = () => {
+  const{id} = useParams();
   const [car, setCar] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -21,4 +23,20 @@ const CarDetails = () => {
       });
     }, [id]
     );
+
+    const handleBooking = () => {
+      axios
+        .post(`https://exploracars.herokuapp.com/api/bookings`, {
+          car_id: car._id,
+          user_id: "some_user_id", 
+          start_date: "some_start_date",
+          end_date: "some_end_date", 
+        });
+        .then((response) => {
+          alert("Your booking request has been sent successfully!");
+        });
+        .catch((error) => {
+          alert("Something went wrong. Please try again later.");
+        });
+      };
 }
